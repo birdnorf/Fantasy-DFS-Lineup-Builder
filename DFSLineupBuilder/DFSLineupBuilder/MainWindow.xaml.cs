58100b15b112463bab3d6104016f87ad50cf0e10
Brady Haider <haid0107@umn.edu>
2015-10-13 00:08:05 -0500

    Added the ability to import and read a CSV file. This is the first step in the process

--- DFSLineupBuilder/DFSLineupBuilder/MainWindow.xaml.cs
+++ DFSLineupBuilder/DFSLineupBuilder/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DFSLineupBuilder.Properties;
+using Microsoft.VisualBasic.FileIO;
+
 
 namespace DFSLineupBuilder
 {
@@ -24,5 +27,59 @@ namespace DFSLineupBuilder
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".csv";
+            dlg.Filter = "CSV File (*.csv)|*.csv|Excel Files (*.xslx)|*.xslx";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                spreadSheetTextBox.Text = filename;
+                parseCSV(filename);
+
+            }
+        }
+
+        public void parseCSV(string filename)
+        {
+            using (TextFieldParser parser = new TextFieldParser(filename))
+            {
+                parser.Delimiters = new string[] { "," };
+                while (true)
+                {
+                    
+                    string[] parts = parser.ReadFields();
+                    if (parts == null)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        foreach(string field in parts)
+                        {
+                            textBlock.Text += string.Format("{0}", field);
+
+                        }
+                    }
+                }
+            }
+        }
+
+
     }
 }
