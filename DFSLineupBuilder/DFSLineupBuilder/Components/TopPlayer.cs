e54b8d889b287f0a5547a60e2f9bbf7235574fca
Brady Haider <haid0107@umn.edu>
2015-10-13 23:08:00 -0500

    performance enhancements

--- DFSLineupBuilder/DFSLineupBuilder/Components/TopPlayer.cs
+++ DFSLineupBuilder/DFSLineupBuilder/Components/TopPlayer.cs
@@ -22,61 +22,40 @@ namespace DFSLineupBuilder.Components
 
             //TODO CHANGE TO SWITCH STATEMENT FOR BETTER PERFORMANCE
 
-
-            //Get Top Center
-            foreach (Player center in listOfPlayers.FindAll(i => i.position == "C"))
+            foreach(Player player in listOfPlayers)
             {
-                if (center.avgPoints > topCenter.avgPoints)
-                    topCenter = center;
-                else
-                    continue;
+                switch (player.position)
+                {
+                    case "C":
+                        if (player.avgPoints > topCenter.avgPoints)
+                            topCenter = player;
+                        continue;
+                    case "RW":
+                        if (player.avgPoints > topRightWing.avgPoints)
+                            topRightWing = player;
+                        continue;
+                    case "LW":
+                        if (player.avgPoints > topLeftWing.avgPoints)
+                            topLeftWing = player;
+                        continue;
+                    case "D":
+                        if (player.avgPoints > topDefense.avgPoints)
+                            topDefense = player;
+                        continue;
+                    case "G":
+                        if (player.avgPoints > topGoalie.avgPoints)
+                            topGoalie = player;
+                        continue;
+                    default:
+                        continue;
+                }
             }
 
             topPlayers.Add(topCenter);
-
-            //Get Top Right Winger
-            foreach (Player rightWing in listOfPlayers.FindAll(i => i.position == "RW"))
-            {
-                if (rightWing.avgPoints > topRightWing.avgPoints)
-                    topRightWing = rightWing;
-                else
-                    continue;
-            }
-
             topPlayers.Add(topRightWing);
-
-            //Get Top Left Winger
-            foreach (Player leftWing in listOfPlayers.FindAll(i => i.position == "LW"))
-            {
-                if (leftWing.avgPoints > topLeftWing.avgPoints)
-                    topLeftWing = leftWing;
-                else
-                    continue;
-            }
-
             topPlayers.Add(topLeftWing);
-            //Get Top Defender
-            foreach (Player defense in listOfPlayers.FindAll(i => i.position == "D"))
-            {
-                if (defense.avgPoints > topDefense.avgPoints)
-                    topDefense = defense;
-                else
-                    continue;
-            }
-
             topPlayers.Add(topDefense);
-            //Get Top Goalie
-            foreach (Player goalie in listOfPlayers.FindAll(i => i.position == "G"))
-            {
-                if (goalie.avgPoints > topGoalie.avgPoints)
-                    topGoalie = goalie;
-                else
-                    continue;
-            }
-
             topPlayers.Add(topGoalie);
-
-
 
             return topPlayers;
         }
